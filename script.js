ff6710e24f7e074e0618269eeafd8ebcaf2f10c3
kulasphere <roden.mrodriguez@gmail.com>
2022-09-19 00:05:31 +0800

    Update files and assets

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-const switchSideBtns = document.querySelectorAll('.switch-side-btn');
-const navbar = document.querySelector('.navbar');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
+const openBtn = document.querySelector("#openBtn");
+const closeBtn = document.querySelector("#closeBtn");
+const toggleBtns = document.querySelectorAll(".toggle");
+const navMenu = document.querySelector("#navMenu");
+const switchBtns = document.querySelectorAll(".switch-btn");
+const navLinks = document.querySelectorAll(".nav__link");
 
-[...switchSideBtns].forEach((btn) => {
-  btn.addEventListener('click', () => {
-    btn.parentElement.parentElement.classList.toggle('change');
+const toggle = () => {
+  openBtn.classList.toggle("hide");
+  closeBtn.classList.toggle("hide");
+
+  if (openBtn.classList.contains("hide")) {
+    navMenu.style.right = "0";
+    document.body.style.overflowY = "hidden";
+  } else {
+    navMenu.style.right = "-100vw";
+    document.body.style.overflowY = "visible";
+  }
+};
+
+toggleBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    toggle();
   });
 });
 
-hamburgerMenu.addEventListener('click', () => {
-  navbar.classList.toggle('change');
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    toggle();
+  });
+});
+
+switchBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.parentElement.classList.toggle("switch");
+  });
+});
+
+const hiddenElems = document.querySelectorAll(".scroll");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("show", entry.isIntersecting);
+    if (entry.isIntersecting) observer.unobserve(entry.target);
+  });
+});
+
+hiddenElems.forEach((el) => {
+  observer.observe(el);
 });
